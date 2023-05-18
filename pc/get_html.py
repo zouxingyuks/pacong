151eda5e3db6b0b2bb81b4dcf099f0be93f4a6ee
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-05-19 00:42:53 +0800

    第二版，成功实现最后成功位置的保留，从而能够进行爬虫的继续

--- pc/get_html.py
+++ pc/get_html.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import time
 import random
-import signal
 import os
 import yaml
 
@@ -14,12 +13,12 @@ Debug_mod = True
 def get_html(city, areaBusiness, browser_type=None):
     headless = not Debug_mod
     counter = 1
-    failed_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'const', 'failed_page.txt')
+    last_success_page = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'const', 'last_success_page.txt')
     config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config', 'config.yaml')
 
-    # 读取失败位置
-    if os.path.exists(failed_file):
-        with open(failed_file, 'r') as f:
+    # 读取最后一次成功的行号
+    if os.path.exists(last_success_page):
+        with open(last_success_page, 'r') as f:
             last_failed_page = int(f.read())
     else:
         last_failed_page = 0
@@ -28,15 +27,6 @@ def get_html(city, areaBusiness, browser_type=None):
     with open(config_file, encoding='utf-8') as f:
         config = yaml.safe_load(f)
 
-    # 设置信号处理函数
-    def handle_signal(signum, frame):
-        with open(failed_file, 'w') as f:
-            f.write(str(page_number))
-        exit(0)
-
-    # 注册终止信号处理函数
-    signal.signal(signal.SIGINT, handle_signal)
-    signal.signal(signal.SIGTERM, handle_signal)
     jobs = []
     with sync_playwright() as p:
         if not browser_type:
@@ -48,7 +38,7 @@ def get_html(city, areaBusiness, browser_type=None):
         else:
             browser = p.webkit.launch(headless=headless)
         context = browser.new_context()
-        # 跳过失败位置之前的页数
+        # 从最后一次成功之后开始
         for page_number in range(last_failed_page + 1, 11):
             url = f'https://www.zhipin.com/web/geek/job?city={city}&areaBusiness={areaBusiness}&page={str(page_number)}'
             page = context.new_page()
@@ -77,10 +67,10 @@ def get_html(city, areaBusiness, browser_type=None):
             counter += 1
             # time.sleep(random.randint(60, 120))
             # 保存失败位置到文件
-            with open(failed_file, 'w') as f:
+            with open(last_success_page, 'w') as f:
                 f.write(str(page_number))
 
             page.close()
-    if os.path.exists(failed_file):
-        os.remove(failed_file)
+    if os.path.exists(last_success_page):
+        os.remove(last_success_page)
     return jobs
--- test.py
+++ test.py
@@ -1,41 +1,25 @@
 import os
-import signal
 from dao.init import save_jobs_to_mysql
 from pc.get_html import get_html
 
-city = None
-area = None
-
 csv_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'const', 'code.csv')
-failed_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'const', 'failed.txt')
+last_success_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'const', 'last_success.txt')
 
-# 读取失败位置
-if os.path.exists(failed_file):
-    with open(failed_file, 'r') as f:
-        last_failed_line = int(f.read())
-else:
-    last_failed_line = 0
+last_success_line = 0
 
-
-# 设置信号处理函数
-def handle_signal(signum, frame):
-    with open(failed_file, 'w') as f:
-        f.write(str(last_failed_line))
-    exit(0)
-
-
-# 注册终止信号处理函数
-signal.signal(signal.SIGINT, handle_signal)
-signal.signal(signal.SIGTERM, handle_signal)
+# 读取最后一次成功的行号
+if os.path.exists(last_success_file):
+    with open(last_success_file, 'r') as f:
+        last_success_line = int(f.read())
 
 try:
     with open(csv_file, encoding='UTF-8') as f:
-        # 跳过失败位置之前的行
-        for _ in range(last_failed_line):
-            next(f)
-
-        line_number = last_failed_line + 1
+        line_number = 1
         for line in f:
+            if line_number <= last_success_line:
+                line_number += 1
+                continue
+
             code, name = line.strip().split(',')
             if code.isdigit() and len(code) == 9:
                 city = {
@@ -47,24 +31,23 @@ try:
                     'id': code,
                     'name': name
                 }
-            if city and area:
-                # 做一些更新 city 和 area 的操作
-                try:
-                    jobs = get_html(city["id"], area["id"])
-                    print(city["id"], area["id"])
-                    print(jobs)
-                    save_jobs_to_mysql(jobs)
-                except Exception as e:
-                    print(f"Failed to import jobs for city '{city['name']}' and area '{area['name']}': {str(e)}")
-                    # 保存失败位置到文件
-                    with open(failed_file, 'w') as f:
-                        f.write(str(line_number))
-                    break  # 终止循环，避免继续处理下一行数据
-            line_number += 1
-            # time.sleep(random.randint(120, 600))
+                if city and area:
+                    # 做一些更新 city 和 area 的操作
+                    try:
+                        jobs = get_html(city["id"], area["id"])
+                        # 测试用代码
+                        # print(city["id"], area["id"])
+                        # print(jobs)
+                        save_jobs_to_mysql(jobs)
+                        last_success_line = line_number
+                        # 保存最后一次成功的行号到文件
+                        with open(last_success_file, 'w') as f:
+                            f.write(str(last_success_line))
+                    except Exception as e:
+                        print(f"Failed to import jobs for city '{city['name']}' and area '{area['name']}': {str(e)}")
+                        break  # 终止循环，避免继续处理下一行数据
 
-    # 处理完成后，删除保存失败位置的文件
-    if os.path.exists(failed_file):
-        os.remove(failed_file)
+            line_number += 1
+
 except FileNotFoundError as e:
     print(f"File not found: {str(e)}")
